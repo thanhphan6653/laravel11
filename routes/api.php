bfae3db733249399790fa4f63905a0a0914e844b
HUY <huyvodinh25102003@gmail.com>
2025-01-09 11:05:06 +0700

    fix routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ClubController;
+use App\Http\Controllers\EnrollmentController;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\StudentDetailController;
 use Illuminate\Http\Request;
@@ -33,7 +34,12 @@ Route::post('/student', [StudentController::class, 'store']);
 Route::put('/student/{id}', [StudentController::class, 'update']);
 Route::delete('/student/{id}', [StudentController::class, 'destroy']);
 
-Route::get('student/{id}/details', [StudentDetailController::class, 'showDetail']);
-Route::post('student/{id}/details', [StudentDetailController::class, 'store']);
-Route::put('student/{id}/details/{detailId}', [StudentDetailController::class, 'update']);
-Route::delete('student/{id}/details/{detailId}', [StudentDetailController::class, 'destroy']);
+Route::get('/student/{id}/details', [StudentDetailController::class, 'showDetail']);
+Route::post('/student/{id}/details', [StudentDetailController::class, 'store']);
+Route::put('/student/{id}/details/{detailId}', [StudentDetailController::class, 'update']);
+Route::delete('/student/{id}/details/{detailId}', [StudentDetailController::class, 'destroy']);
+
+Route::get('/enrollment', [EnrollmentController::class, 'index']);
+Route::post('/enrollment', [EnrollmentController::class, 'store']);
+Route::put('/enrollment/{id}', [EnrollmentController::class, 'update']);
+Route::delete('/enrollment/{id}', [EnrollmentController::class, 'destroy']);
